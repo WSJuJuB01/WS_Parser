ee3be20e33d8d452261e1fa7b3da8ad211a18ef0
WSJuJuB01 <Vladosius228@yandex.ru>
2026-05-02 23:18:51 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -9,6 +9,7 @@ SOURCES = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_SS+All_RUS.txt",
     "https://raw.githubusercontent.com/RKPchannel/RKP_bypass_configs/refs/heads/main/configs/url_work.txt",
     "https://raw.githubusercontent.com/Ilyacom4ik/free-v2ray-2026/refs/heads/main/subscriptions/FreeCFGHub1.txt"
+    "https://etoneya.vercel.app/whitelist"
 ]
 
 # Словарь для самых популярных стран (чтобы названия были красивыми)
